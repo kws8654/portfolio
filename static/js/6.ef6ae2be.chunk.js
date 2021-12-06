d00e41f5dbf88995dd50def2737913b2e168e93a
qws3433 <kws8654@naver.com>
2021-12-07 04:03:58 +0900

    Updates

---
+++ static/js/6.ef6ae2be.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmacfolio=this.webpackJsonpmacfolio||[]).push([[6],{39:function(a,o,s){"use strict";s.r(o);s(0);var c=s(1);o.default=function(a){var o=a.className;return Object(c.jsx)("video",{className:o,src:"./image/PF1.mov",alt:"",autoPlay:!0,loop:!0})}}}]);
+//# sourceMappingURL=6.ef6ae2be.chunk.js.map
